7d3865c6aed6ef7dd6c784ed29f21ca761e73a38
gyurikol <gyuri.kol@gmail.com>
2018-08-08 20:25:02 -0700

    icon and porject name change

--- SimpleHelicalSpline/Form1.Designer.cs
+++ SimpleHelicalSpline/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -162,6 +163,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMain";
             this.Text = "CAD CAM Services - Helix";
             this.ResumeLayout(false);
